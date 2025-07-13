5adcac3db22df6e75557e455930bbfadb45b9501
Oliver Hawkins <oli@olihawkins.com>
2025-07-13 22:15:02 +0100

    Started work integrating handlers for child elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
+
+// Imports --------------------------------------------------------------------
+
 import TactileSvg from "./lib/TactileSvg.svelte";
+
+// Handlers -------------------------------------------------------------------
+
+let fillColor = $state("red");
+
+function onClick(e: Event) {
+  fillColor = fillColor === "red" ? "green" : "red";
+  e.preventDefault(); 
+}
+
 </script>
 
 <TactileSvg>
-  <circle cx="50" cy="50" r="5" fill="red" />
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_static_element_interactions -->
+  <circle 
+    onmouseenter={onClick} 
+    onmouseout={onClick} 
+    onblur={onClick}
+    cx="50" 
+    cy="50" 
+    r="5" 
+    fill={fillColor} />
   <rect x="20" y="20" width="10" height="10" fill="blue" />
 </TactileSvg>
